4cf314be175bb495e4c9436db6e4fe695f5729cb
Thomaz Machado <thomazcm@gmail.com>
2025-12-13 10:09:47 -0300

    Initial commit

---
+++ src/main/java/com/thomaz/ProxyApplication.java
@@ -0,0 +1,21 @@
+package com.thomaz;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
+
+@SpringBootApplication
+public class ProxyApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxyApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/thomaz/api/BaseEndpoint.java
@@ -0,0 +1,11 @@
+package com.thomaz.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class BaseEndpoint {
+
+
+}
